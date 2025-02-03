0356f972c526123b7198989a9eb90c6919ab80e8
codgician <15964984+codgician@users.noreply.github.com>
2025-02-03 22:35:55 +0800

    pkgs: auto export pkgs by folder name

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,12 +1,11 @@
 { pkgs, ... }:
-let
-  callPackage = pkgs.lib.callPackageWith (pkgs // mypkgs);
-  mypkgs = {
-    aiursoft-tracer = callPackage ./aiursoft-tracer { };
-    mtk_uartboot = callPackage ./mtk_uartboot { };
-    tat-agent = callPackage ./tat-agent { };
-  };
+with pkgs.lib; let 
+  callPackage = callPackageWith (pkgs // mypkgs);
+  mypkgs = pipe (builtins.readDir ./.) [
+    (filterAttrs (_: type: type == "directory"))
+    (mapAttrs (k: v: callPackage ./${k} { }))
+  ];
 in
-pkgs.lib.filterAttrs (
+filterAttrs (
   k: v: !(v.meta ? platforms) || (builtins.elem pkgs.system v.meta.platforms)
 ) mypkgs
